2737d9d9627b91b5c406242c4003bb65cf7d02e8
Julian Salcedo <julianpeloz@gmail.com>
2022-07-14 17:00:26 -0300

    Adding vite + react and firebase config

---
+++ src/providers/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+// import all the instances to the products
+import { getAuth } from "firebase/auth";
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID,
+} = import.meta.env
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Adding Build products to app
+
+export {
+  app,
+};
